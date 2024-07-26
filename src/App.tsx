f8ceb563127cf9c7f979371961bfb80001727ac0
siunami <siunami.matt@gmail.com>
2024-07-25 20:06:36 -0700

    started prompting workflow

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
 	Tldraw,
 	// TLComponents,
 	TLBaseShape,
-	HTMLContainer,
+	// HTMLContainer,
 	ShapeUtil,
 	Rectangle2d,
 	track,
@@ -22,6 +22,14 @@ import {
 	TLRecord,
 	useToasts,
 	TLUiIconType,
+	TLComponents,
+	// DefaultQuickActions,
+	TldrawUiMenuItem,
+	DefaultToolbarContent,
+	// useIsToolSelected,
+	useTools,
+	DefaultToolbar,
+	// DefaultQuickActionsContent,
 	// TLOnBeforeUpdateHandler,
 } from "tldraw";
 import "tldraw/tldraw.css";
@@ -104,6 +112,88 @@ const SearchQueryContext = createContext<
 // 	NavigationPanel: CustomNavigationPanel, // null will hide the panel instead
 // };
 
+type PromptShape = TLBaseShape<
+	"prompt",
+	{
+		w: number;
+		h: number;
+		text: string;
+	}
+>;
+
+class PromptShapeUtil extends ShapeUtil<PromptShape> {
+	static override type = "prompt" as const;
+
+	getDefaultProps() {
+		return {
+			w: 250,
+			h: 120,
+			text: "",
+		};
+	}
+
+	getGeometry(shape: PromptShape) {
+		return new Rectangle2d({
+			width: shape.props.w,
+			height: shape.props.h,
+			isFilled: true,
+		});
+	}
+
+	override canBind({}: TLShapeUtilCanBindOpts<PromptShape>) {
+		// Allow pins to participate in other bindings, e.g. arrows
+		return true;
+	}
+
+	component(shape: PromptShape) {
+		return (
+			<div
+				ref={(el) => {
+					if (el) {
+						const newHeight = el.getBoundingClientRect().height;
+						if (newHeight !== shape.props.h) {
+							// Update the shape's height in the editor
+							this.editor?.updateShape({
+								id: shape.id,
+								type: "card",
+								props: {
+									...shape.props,
+									h: newHeight,
+								},
+							});
+						}
+					}
+				}}
+				className="card-container"
+				style={{
+					pointerEvents: "all",
+					minHeight: "90px",
+				}}
+			>
+				<h2
+					style={{
+						margin: 0,
+						color: "grey",
+					}}
+				>
+					Prompt
+				</h2>
+				{shape.props.text}
+			</div>
+		);
+	}
+
+	indicator(shape: PromptShape) {
+		return (
+			<rect
+				style={{ borderRadius: "3px" }}
+				width={shape.props.w}
+				height={shape.props.h}
+			/>
+		);
+	}
+}
+
 //
 type CardShape = TLBaseShape<
 	"card",
@@ -127,7 +217,7 @@ class CardShapeUtil extends ShapeUtil<CardShape> {
 	getDefaultProps(): CardShape["props"] {
 		return {
 			w: 250,
-			h: 120,
+			h: 90,
 			feature: "10138",
 			description: "related to London",
 			fromFeature: "",
@@ -161,70 +251,96 @@ class CardShapeUtil extends ShapeUtil<CardShape> {
 								);
 							}
 							return (
-								<HTMLContainer
+								<div
+									ref={(el) => {
+										if (el) {
+											const newHeight = el.getBoundingClientRect().height;
+											if (newHeight !== shape.props.h) {
+												// Update the shape's height in the editor
+												this.editor?.updateShape({
+													id: shape.id,
+													type: "card",
+													props: {
+														...shape.props,
+														h: newHeight,
+													},
+												});
+											}
+										}
+										// setTimeout(() => {
+										// 	console.log(shape.props.feature);
+										// 	console.log(shape.props.description);
+										// 	console.log(el?.getBoundingClientRect());
+										// 	const height = el?.getBoundingClientRect().height;
+										// 	this.editor?.updateShape({
+										// 		id: shape.id,
+										// 		type: "card",
+										// 		props: {
+										// 			...shape.props,
+										// 			h: height ? height + 10 : 90,
+										// 		},
+										// 	});
+										// }, 200);
+										// this.element = el;
+										// setTimeout(() => {
+										// 	console.log("Feature: " + shape.props.feature);
+										// 	console.log(el);
+										// 	if (el && !this.measurementDone) {
+										// 		const height = el.getBoundingClientRect().height;
+										// 		// + 10 considers padding of parent element
+										// 		const currentHeight = height + 10;
+										// 		console.log(el);
+										// 		console.log(el.getBoundingClientRect());
+										// 		console.log("current height: " + currentHeight);
+										// 		console.log("shape height: " + shape.props.h);
+										// 		this.editor?.updateShape({
+										// 			id: shape.id,
+										// 			type: "card",
+										// 			props: {
+										// 				...shape.props,
+										// 				h: Math.max(currentHeight, MIN_CARD_HEIGHT),
+										// 			},
+										// 		});
+										// 		this.measurementDone = true;
+										// 	}
+										// }, 200);
+									}}
 									className="card-container"
 									style={{
 										pointerEvents: "all",
+										minHeight: "90px",
 									}}
 								>
-									<div
-									// ref={(el) => {
-									// 	this.element = el;
-									// 	setTimeout(() => {
-									// 		console.log("Feature: " + shape.props.feature);
-									// 		console.log(el);
-									// 		if (el && !this.measurementDone) {
-									// 			const height = el.getBoundingClientRect().height;
-									// 			// + 10 considers padding of parent element
-									// 			const currentHeight = height + 10;
-									// 			console.log(el);
-									// 			console.log(el.getBoundingClientRect());
-									// 			console.log("current height: " + currentHeight);
-									// 			console.log("shape height: " + shape.props.h);
-									// 			this.editor?.updateShape({
-									// 				id: shape.id,
-									// 				type: "card",
-									// 				props: {
-									// 					...shape.props,
-									// 					h: Math.max(currentHeight, MIN_CARD_HEIGHT),
-									// 				},
-									// 			});
-									// 			this.measurementDone = true;
-									// 		}
-									// 	}, 200);
-									// }}
-									>
-										<div className="card-row">
-											<h2 style={{ margin: 0 }}>{shape.props.feature}</h2>
-											<div
-												className="card-preview"
-												onClick={() => {
-													featureContext.setFeatureNumber(
-														Number(shape.props.feature)
-													);
-													searchContext.setSearchQuery(shape.props.description);
-												}}
-												onPointerDown={(e) => e.stopPropagation()}
-												onTouchStart={(e) => e.stopPropagation()}
-												onTouchEnd={(e) => e.stopPropagation()}
-											>
-												🔍
-											</div>
-											<div className="card-stats">
-												<div>
-													{shape.props.fromFeature !== ""
-														? `${Number(shape.props.score)
-																.toFixed(2)
-																.replace(/^0\./, ".")} ${
-																shape.props.type
-														  } for ${shape.props.fromFeature}`
-														: ""}
-												</div>
+									<div className="card-row">
+										<h2 style={{ margin: 0 }}>{shape.props.feature}</h2>
+										<div
+											className="card-preview"
+											onClick={() => {
+												featureContext.setFeatureNumber(
+													Number(shape.props.feature)
+												);
+												searchContext.setSearchQuery(shape.props.description);
+											}}
+											onPointerDown={(e) => e.stopPropagation()}
+											onTouchStart={(e) => e.stopPropagation()}
+											onTouchEnd={(e) => e.stopPropagation()}
+										>
+											🔍
+										</div>
+										<div className="card-stats">
+											<div>
+												{shape.props.fromFeature !== ""
+													? `${Number(shape.props.score)
+															.toFixed(2)
+															.replace(/^0\./, ".")} ${shape.props.type} for ${
+															shape.props.fromFeature
+													  }`
+													: ""}
 											</div>
 										</div>
-										<span>{shape.props.description}</span>
 									</div>
-								</HTMLContainer>
+									<span>{shape.props.description}</span>
+								</div>
 							);
 						}}
 					</SearchQueryContext.Consumer>
@@ -244,7 +360,7 @@ class CardShapeUtil extends ShapeUtil<CardShape> {
 	}
 }
 
-const customShape = [CardShapeUtil];
+const customShape = [CardShapeUtil, PromptShapeUtil];
 
 type ArrowDirection = "right" | "down" | "left" | "up";
 
@@ -279,19 +395,22 @@ const CustomUi = track(() => {
 	const editor = useEditor();
 	const { addToast, removeToast } = useToasts();
 
-	const handleAddInfoToast = ({
+	const createToast = ({
 		title,
 		description,
 		icon = "dots-horizontal",
+		severity = "info",
 	}: {
 		title: string;
 		description: string;
 		icon?: TLUiIconType;
+		severity?: "info" | "error" | "success";
 	}) => {
 		return addToast({
 			title: title,
 			description: description,
 			icon: icon,
+			severity: severity,
 			actions: [
 				{
 					label: "Dismiss",
@@ -333,10 +452,11 @@ const CustomUi = track(() => {
 	const addFeaturesToGraph = async (type: Retrieval) => {
 		const selectedShapes = editor?.getSelectedShapeIds();
 		if (!selectedShapes || selectedShapes.length !== 1) {
-			const errorToastId = handleAddInfoToast({
+			const errorToastId = createToast({
 				title: "Error",
-				description: "Please select exactly one card.",
+				description: "Please select exactly one feature.",
 				icon: "cross-2",
+				severity: "error",
 			});
 			setTimeout(() => {
 				removeToast(errorToastId);
@@ -345,10 +465,11 @@ const CustomUi = track(() => {
 		}
 		const shape: CardShape | undefined = editor?.getShape(selectedShapes[0]);
 		if (!shape || shape.type !== "card") {
-			const errorToastId = handleAddInfoToast({
+			const errorToastId = createToast({
 				title: "Error",
-				description: "Selected shape is not a card.",
+				description: "Selected item is not a feature.",
 				icon: "cross-2",
+				severity: "error",
 			});
 			setTimeout(() => {
 				removeToast(errorToastId);
@@ -359,7 +480,7 @@ const CustomUi = track(() => {
 
 		const topK = 10;
 
-		const toastId = handleAddInfoToast({
+		const toastId = createToast({
 			title: "Fetching",
 			description: "Fetching " + type + " for " + shape.props.feature,
 		});
@@ -444,7 +565,7 @@ const CustomUi = track(() => {
 						type: "arrow",
 						x: 150,
 						y: 150,
-						// isLocked: true,
+						isLocked: true,
 						props: {
 							// @ts-ignore
 							text: Number(values[index]).toFixed(2).replace(/^0\./, "."),
@@ -490,10 +611,11 @@ const CustomUi = track(() => {
 			removeToast(toastId);
 		} catch (error: any) {
 			removeToast(toastId);
-			const errorToastId = handleAddInfoToast({
+			const errorToastId = createToast({
 				title: "Error",
 				description: `Failed to fetch ${type} data: ${error.message}`,
 				icon: "cross-2",
+				severity: "error",
 			});
 			setTimeout(() => {
 				removeToast(errorToastId);
@@ -503,28 +625,139 @@ const CustomUi = track(() => {
 
 	useEffect(() => {
 		async function handleKeyDown(e: KeyboardEvent) {
-			// Check for Cmd + Shift + key combination
 			if (e.shiftKey) {
+				const selectedShapes = editor?.getSelectedShapes();
+
+				console.log(selectedShapes);
+
+				const steering = selectedShapes.filter(
+					(shape) => shape.type === "card"
+				);
+				const prompt = selectedShapes.filter(
+					(shape) => shape.type === "prompt"
+				);
+
 				switch (e.key) {
 					case "J":
-						console.log("Cmd + Shift + j pressed");
-						// Add your custom action here
+						console.log("Shift + j pressed");
 						await addFeaturesToGraph(RETRIEVAL_TYPES.COSINE_SIM);
 						break;
 					case "U":
-						console.log("Cmd + Shift + U pressed");
-						// Add your custom action here
+						console.log("Shift + U pressed");
 						await addFeaturesToGraph(RETRIEVAL_TYPES.ACTIONS);
 						break;
 					case "I":
-						console.log("Cmd + Shift + I pressed");
-						// Add your custom action here
+						console.log("Shift + I pressed");
 						await addFeaturesToGraph(RETRIEVAL_TYPES.EFFECTS);
 						break;
 					case "K":
-						console.log("Cmd + Shift + k pressed");
-						// Add your custom action here
+						console.log("Shift + k pressed");
 						await addFeaturesToGraph(RETRIEVAL_TYPES.CO_OCCURRING_EFFECTS);
+						break;
+					case "P":
+						console.log("Shift + p pressed");
+						// Add a success toast
+						const promptToast = createToast({
+							title: "Success",
+							description: "This will eventually generate text",
+							icon: "check",
+							severity: "success",
+						});
+						setTimeout(() => {
+							removeToast(promptToast);
+						}, 3000);
+						break;
+					case "S":
+						console.log("Shift + s pressed");
+
+						if (prompt.length < 1) {
+							createToast({
+								title: "Error",
+								description: "Please select a prompt.",
+								icon: "cross-2",
+								severity: "error",
+							});
+							return;
+						}
+
+						if (prompt.length > 1) {
+							createToast({
+								title: "Error",
+								description: "Please select only one prompt.",
+								icon: "cross-2",
+								severity: "error",
+							});
+							return;
+						}
+
+						if (steering.length < 1) {
+							createToast({
+								title: "Error",
+								description: "Please select at least one feature.",
+								icon: "cross-2",
+								severity: "error",
+							});
+							return;
+						}
+
+						// Create arrows from each feature to the prompt
+						steering.forEach((featureShape) => {
+							const arrow_id = createShapeId();
+
+							editor?.createShapes([
+								{
+									id: arrow_id,
+									type: "arrow",
+									x: 150,
+									y: 150,
+									isLocked: true,
+									props: {
+										text: "", // You can add text here if needed
+									},
+									meta: {
+										fromId: featureShape.id,
+										toId: prompt[0].id,
+									},
+								},
+							]);
+
+							editor.createBindings([
+								{
+									fromId: arrow_id,
+									toId: featureShape.id,
+									type: "arrow",
+									props: {
+										terminal: "start",
+										normalizedAnchor: { x: 1, y: 0.5 },
+										isExact: false,
+										isPrecise: true,
+									},
+								},
+								{
+									fromId: arrow_id,
+									toId: prompt[0].id,
+									type: "arrow",
+									props: {
+										terminal: "end",
+										normalizedAnchor: { x: 0, y: 0.5 },
+										isExact: false,
+										isPrecise: true,
+									},
+								},
+							]);
+						});
+
+						// Add a success toast
+						const toastID = createToast({
+							title: "Success",
+							description: "Arrows created from features to prompt.",
+							icon: "check",
+							severity: "success",
+						});
+						setTimeout(() => {
+							removeToast(toastID);
+						}, 3000);
+
 						break;
 				}
 			}
@@ -565,6 +798,67 @@ const CustomUi = track(() => {
 		</div>
 	);
 });
+
+function CustomToolbar() {
+	const editor = useEditor();
+	const tools = useTools();
+
+	const [selected, setSelected] = useState(false);
+	const [text, setText] = useState("");
+
+	const handleSubmit = () => {
+		// Add your submit logic here
+		const { x, y, width, height } = editor.getViewportPageBounds();
+		const centerX = x + width / 2 - 120;
+		const centerY = y + height * 0.8;
+
+		editor.createShapes([
+			{
+				id: createShapeId(),
+				type: "prompt",
+				x: centerX,
+				y: centerY,
+				props: {
+					text,
+				},
+			},
+		]);
+		setText(""); // Clear the input after submission
+		setSelected(false);
+	};
+
+	return (
+		<div>
+			<DefaultToolbar>
+				<TldrawUiMenuItem
+					id="code"
+					icon="edit"
+					onSelect={() => setSelected(!selected)}
+				/>
+
+				{selected && (
+					<input
+						type="text"
+						value={text}
+						autoFocus
+						onChange={(e) => setText(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								e.preventDefault();
+								handleSubmit();
+							}
+						}}
+					/>
+				)}
+				<DefaultToolbarContent />
+			</DefaultToolbar>
+		</div>
+	);
+}
+
+const components: TLComponents = {
+	Toolbar: CustomToolbar,
+};
 
 function App() {
 	const [featureNumber, setFeatureNumber] = useState(10138);
@@ -666,7 +960,7 @@ function App() {
 			<SearchQueryContext.Provider value={{ searchQuery, setSearchQuery }}>
 				<div style={{ position: "fixed", inset: 0 }}>
 					<Tldraw
-						// components={components}
+						components={components}
 						shapeUtils={customShape}
 						onMount={(editor: Editor) => {
 							editor.sideEffects.registerAfterDeleteHandler(
@@ -686,29 +980,13 @@ function App() {
 											});
 
 										connections.forEach((connection: TLRecord) => {
-											console.log(connection);
-											// @ts-ignore
-											editor.deleteShape(connection.id);
-											// editor.updateShape(
-											// 	// @ts-ignore
-											// 	Object.assign(connection, {
-											// 		isLocked: false,
-											// 	})
-											// );
-											// editor.updateShape({
-											// 	// @ts-ignore
-											// 	id: connection.id,
-											// 	type: "arrow",
-											// 	isLocked: false,
-											// });
-
-											// editor.run(
-											// 	() => {
-											// 		// @ts-ignore
-											// 		editor.deleteShape(connection.id);
-											// 	},
-											// 	{ ignoreShapeLock: true }
-											// );
+											(editor as Editor).run(
+												() => {
+													// @ts-ignore
+													editor.deleteShape(connection.id);
+												},
+												{ ignoreShapeLock: true }
+											);
 										});
 									}
 
@@ -723,19 +1001,6 @@ function App() {
 										}
 									);
 
-									// // grab the parent of the shape and check if it's a frame:
-									// const parentShape = editor.getShape(shape.parentId);
-									// if (parentShape && parentShape.type === "frame") {
-									// 	// if it is, get the IDs of all its remaining children:
-									// 	const siblings = editor.getSortedChildIdsForParent(
-									// 		parentShape.id
-									// 	);
-
-									// 	// if there are none (so the frame is empty), delete the frame:
-									// 	if (siblings.length === 0) {
-									// 		editor.deleteShape(parentShape.id);
-									// 	}
-									// }
 									return;
 								}
 							);
